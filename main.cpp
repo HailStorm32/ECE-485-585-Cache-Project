70a452619ab82f75ce133fbe2800b63ed936ddaa
Demetri <demetriv.s.7@gmail.com>
2023-02-25 10:08:04 -0800

    Formated main

--- main.cpp
+++ main.cpp
@@ -2,38 +2,51 @@
 #include <string>
 #include "cache.h"
 
+#define INST_L1_WAYS		4
+#define INST_L1_SETS		16000
+#define INST_L1_LINE_SIZE	64
+
+#define DATA_L1_WAYS		8
+#define DATA_L1_SETS		16000
+#define DATA_L1_LINE_SIZE	64
+
 int main()
-{
-	//Below is just testing code, feel free to delete
-	std::string test;
+{	
+	Cache dataL1(DATA_L1_WAYS, DATA_L1_SETS, DATA_L1_LINE_SIZE);
+	Cache instL1(INST_L1_WAYS, INST_L1_SETS, INST_L1_LINE_SIZE);
 
-	Cache S1(4, 16000, 64);
+	
+	switch (command)
+	{
+	case 0:
+		//Function 0 here
+		break;
+	case 1:
+		//Function 1 here
+		break;
+	case 2:
+		//Function 2 here
+		break;
+	case 3:
+		//Funtion 3 here
+		break;
+	case 4:
+		//Function 4 here
+		break;
+	case 8:
+		//Function 8 here
+		break;
+	case 9:
+		//Functoin 9 here
+		break;
+	default:
+		std::cout << "\n\nERROR: Invalid command " << command << "\n\n";
+		break;
+	}
 
-	cacheLinePtr_t line = S1.returnLine(0, 0);
-
-	line->tag = 323;
-	line->MESI = MODIFIED;
-	S1.updateLRU(line);
-
-	line = S1.returnLine(0, 1);
-
-	line->tag = 500;
-	S1.updateLRU(line);
-
-	S1.testPrintSet(0);
-	S1.testPrintSet(1);
-
-	line = S1.getNextAvailLine(0);
-
-	line->tag = 400;
-	S1.updateLRU(line);
-
-	S1.testPrintSet(0);
-	S1.testPrintSet(1);
-
-	std::cout << "HELLO" << std::endl;
-
-	std::cin >> test;
+	//Hold terminal open until user exits
+	std::cout << "Press <Enter> to close..." << std::endl;
+	std::cin.get();
 
 	return 0;
 }
