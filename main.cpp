628af02f174300ae1d37b88b4aecfe4f5e3db706
Demetri <demetriv.s.7@gmail.com>
2023-02-25 11:59:06 -0800

    Added address parser

--- main.cpp
+++ main.cpp
@@ -12,10 +12,17 @@
 #define DATA_L1_SETS		16000
 #define DATA_L1_LINE_SIZE	64
 
+void addrParser(uint32_t address, uint16_t* tag, uint16_t* setID);
+
 int main()
 {	
 	Cache dataL1(DATA_L1_WAYS, DATA_L1_SETS, DATA_L1_LINE_SIZE);
 	Cache instL1(INST_L1_WAYS, INST_L1_SETS, INST_L1_LINE_SIZE);
+
+	uint16_t tag = 0;
+	uint16_t setID = 0;
+
+	
 
 	std::ifstream infile("test.txt");
 
@@ -24,6 +31,9 @@ int main()
 
 	while(infile >> command >> address_str){
 		address_hex = std::stoi(address_str, 0, 16);
+
+		addrParser(address_hex, &tag, &setID);
+
 		switch (command)
 		{
 		case 0:
@@ -58,4 +68,19 @@ int main()
 	std::cin.get();
 
 	return 0;
+}
+
+void addrParser(uint32_t address, uint16_t *tag, uint16_t *setID)
+{
+	//Make sure the varibles we are using for output are "clean"
+	*tag = 0;
+	*setID = 0;
+
+	//Mask bits [31:20] and [5:0] then right shift 6bits to get set
+	*setID = (address & 0xFFFC0) >> 6;
+
+	//Mask bits [19:6] and [5:0] then right shift 20bits to get tag
+	*tag = (address & 0xFFF00000) >> 20;
+
+	std::cout << "Set: " << *setID << " | " << "Tag: " << *tag << std::endl;
 }
